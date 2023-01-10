c0b3bf927e50c0d9746ebe72e6a420efdf4abb03
VTRyo <vtryo.work@gmail.com>
2023-01-11 02:07:18 +0900

    aaaaaaaaa

--- .github/workflows/scripts/prh.sh
+++ .github/workflows/scripts/prh.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-prh --rules prh/prh.yml --verify article-sample/*/*.md > prh.log || \
-EXIT_CODE=$?
-
-cat prh.log | bin/reviewdog -reporter=github-pr-review \
-  -diff='git --no-pager diff origin/main' \
-  -efm='%f(%l,%c): %m' \
-  -name=prh
-
-exit ${EXIT_CODE}
+prh --rules prh/prh.yml --verify article-sample/*/*.md > prh.log
