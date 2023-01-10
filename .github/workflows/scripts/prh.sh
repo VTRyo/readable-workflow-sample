44bdc8f2634d2153b151be48d7694e5f296402f0
VTRyo <vtryo.work@gmail.com>
2023-01-10 23:26:20 +0900

    add reviewdog

---
+++ .github/workflows/scripts/prh.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+prh --rules prh.yml --verify article-sample/*/*.md > prh.log || \
+EXIT_CODE=$?
+
+cat prh.log | bin/reviewdog -reporter=github-pr-review \
+  -diff='git --no-pager diff origin/main' \
+  -efm='%f(%l,%c): %m' \
+  -name=prh
+
+exit ${EXIT_CODE}
---
+++ .github/workflows/scripts/textlint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+textlint --format checkstyle --config .textlintrc article-sample/*/*.md > \
+textlint.log || EXIT_CODE=$?
+
+cat textlint.log | bin/reviewdog -reporter=github-pr-review \
+  -diff='git --no-pager diff origin/main' \
+  -f=checkstyle \
+  -name=textlint
+
+exit ${EXIT_CODE}
